fc52a859d82f5669ebc1232109d250f9bb8bdb9f
Lasse <lass.osmann@gmail.com>
2024-01-28 19:52:15 +0100

    g

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,9 +3,16 @@ import { ScrollAnimation } from "../Animations/ScrollAnimation";
 
 export default function Footer() {
   return (
-    <footer className="py-[100px]">
+    <footer className="py-[100px] pb-[20px]">
       <div className="w-[90%] mx-auto max-w-main">
-        <h1>footer</h1>
+        <div className="grid grid-cols-3"></div>
+        <div className="border-t-[1px] flex items-center justify-between py-[20px] text-gray-400 text-[14px] font-[300]">
+          <h1>
+            COAD Agency | © Alle rettigheder forbeholdt,{" "}
+            {new Date().getFullYear()}
+          </h1>
+          <h1>CVR: 43407376</h1>
+        </div>
       </div>
     </footer>
   );
