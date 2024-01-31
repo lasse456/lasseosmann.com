2348fa49cfd613c23a5f41b0ca5fbb17c7553bca
Lasse <lass.osmann@gmail.com>
2024-01-31 23:47:23 +0100

    heor

--- src/components/language.tsx
+++ src/components/language.tsx
@@ -8,36 +8,46 @@ export default function Language() {
   const [isEnglish, setIsEnglish] = useState(false);
   return (
     <div className="flex items-center gap-[4px] bg-[#f0f2f5] p-[4px] rounded-[100px]">
-      <a
-        href="/"
+      <Link
+        href={"/"}
         className={
           currentPath === "/en"
             ? "p-[6px] flex items-center gap-[6px] rounded-[70px] cursor-pointer"
             : "bg-white p-[6px] flex items-center gap-[6px] rounded-[70px] cursor-pointer"
         }
         onClick={() => {
-          setIsEnglish(false);
-          console.log("Button clicked");
+          // Use router.push() to navigate to the new route
+          router.push("/en");
+
+          // Use router.reload() to reload the page after a short delay (adjust as needed)
+          setTimeout(() => {
+            router.reload();
+          }, 500); // 500 milliseconds delay, adjust as needed
         }}
       >
         <img className="w-[16px]" src="/denmark.png"></img>
         <p className="text-[13px]">DK</p>
-      </a>
-      <a
-        href="/en"
+      </Link>
+      <Link
+        href={"/en"}
         className={
           currentPath === "/en"
             ? "bg-white p-[6px] flex items-center gap-[6px] rounded-[70px] cursor-pointer"
             : " p-[6px] flex items-center gap-[6px] rounded-[70px] cursor-pointer"
         }
         onClick={() => {
-          setIsEnglish(true);
-          console.log("Button clicked");
+          // Use router.push() to navigate to the new route
+          router.push("/en");
+
+          // Use router.reload() to reload the page after a short delay (adjust as needed)
+          setTimeout(() => {
+            router.reload();
+          }, 500); // 500 milliseconds delay, adjust as needed
         }}
       >
         <img className="w-[16px]" src="/united-kingdom.png"></img>
         <p className="text-[13px]">EN</p>
-      </a>
+      </Link>
     </div>
   );
 }
