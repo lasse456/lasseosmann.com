15d0c56e0f30e3d45e91fc1564d6647878f1833f
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-08 23:21:17 +0100

    l

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <Layout>
       <img className="w-[80px] rounded-full mb-[30px]" src="/lasse.png"></img>
-      <h1 className="text-[46px] font-[600] text-gray-800 mb-[10px]">Hello, I'm Lasse 👋</h1>
+      <h1 className="text-[46px] font-[600] text-gray-800 mb-[10px]">Hello, I&apos;m Lasse 👋</h1>
       <p className="font-[400] text-[20px] text-gray-600 mb-[30px]">A self-taught 17-year old software developer and digital product designer from Denmark</p>
       <div className="flex items-center gap-[14px] mb-[70px]">
         <Link target="_blank" href={"https://www.linkedin.com/in/lasse-osmann-%F0%9F%91%8B-1b6ab9243/"}>
@@ -25,15 +25,15 @@ export default function Home() {
           <FaInstagram size={24} color="rgb(75 85 99)" />
         </Link>
       </div>
-      <p className="font-[400] text-gray-600 mb-[100px]">37signals is the company behind Basecamp, HEY, and ONCE. I own that business together with Jason Fried. We've been around since 1999.
+      <p className="font-[400] text-gray-600 mb-[100px]">37signals is the company behind Basecamp, HEY, and ONCE. I own that business together with Jason Fried. We&apos;ve been around since 1999.
 <br></br><br></br>
-Basecamp is the one-place-not-all-over-the-place way to organize your projects and your company. Launched in 2004 as one of the first software-as-a-service applications, and used by millions of people since. It's the original Rails application.
+Basecamp is the one-place-not-all-over-the-place way to organize your projects and your company. Launched in 2004 as one of the first software-as-a-service applications, and used by millions of people since. It&apos;s the original Rails application.
 <br></br><br></br>
-HEY is a fresh approach to turning email into something you want to use, not something you’re forced to deal with. A complete alternative to Gmail or Outlook for individuals and companies who want a better relationship with email. Tens of thousands of customers have embraced it since we launched in 2020.
+HEY is a fresh approach to turning email into something you want to use, not something you&apos;re forced to deal with. A complete alternative to Gmail or Outlook for individuals and companies who want a better relationship with email. Tens of thousands of customers have embraced it since we launched in 2020.
 <br></br><br></br>
 ONCE is a return to software products, away from services, monthly subscriptions, and shared data. Software you can own with source code you can read and change. Our first offering is Campfire, super simple group chat for a one-time price.</p>
     
-    <h1 className="text-[30px] font-[600] text-gray-800 mb-[70px]">Products I'm working on 💼</h1>
+    <h1 className="text-[30px] font-[600] text-gray-800 mb-[70px]">Products I&apos;m working on 💼</h1>
     <div className="flex gap-[20px]">
       <img className="w-[80px] h-[80px] object-cover rounded-full" src="/projects/toolbird.svg"></img>
       <div>
